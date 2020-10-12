4a8a22270f24da9e52d75e4720a3c82f39c6c9aa
gacuchac <gabriel.cuchacovich@gmail.com>
2020-10-11 23:09:27 -0300

    Toronto Routes

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -34,12 +34,10 @@ let airportData = "https://raw.githubusercontent.com/gacuchac/Mapping_Earthquake
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
-  //  console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
   L.geoJson(data, {
     onEachFeature: function(feature, layer) {
-        console.log(layer);
         layer.bindPopup("<h2>Airport Code: " + layer.feature.properties.faa + "</h2> <hr> <h3>Airport Name: " + layer.feature.properties.name + "</h3>");
      }
     }).addTo(map);
